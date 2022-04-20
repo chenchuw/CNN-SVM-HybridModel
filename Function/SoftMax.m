cfdaff5d5140a6bc72e7cb5aab189cd9ee124b27
Mockingbird <chenchuw@bu.edu>
2022-04-19 22:06:15 -0400

    Code Refactoring & Make it attachable to SVM

--- Function/SoftMax.m
+++
@@ -1,5 +0,0 @@
-function [probs] = SoftMax(Weight, Bias, Layer)
-    probs = exp(bsxfun(@plus, Weight * Layer, Bias));
-    sumProbs = sum(probs, 1);
-    probs = bsxfun(@times, probs, 1 ./ sumProbs);
-end

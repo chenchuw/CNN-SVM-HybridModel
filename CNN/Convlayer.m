53857e95cb3153c2347c7d199c4d70f380861e09
Woody-Wan <woodyallen1999@live.com>
2022-05-03 16:15:12 -0400

    tuned

--- CNN/Convlayer.m
+++ CNN/Convlayer.m
@@ -1,7 +1,7 @@
-function Featuremap = Convlayer(images,W,b)
-    filterDim = size(W,1);
-    numFilters1 = size(W,3);
-    numFilters2 = size(W,4);
+function Featuremap = Convlayer(images,Weight,Bias)
+    filterDim = size(Weight,1);
+    numFilters1 = size(Weight,3);
+    numFilters2 = size(Weight,4);
     numImages = size(images,4);
     imageDim = size(images,1);
     convDim = imageDim-filterDim+1;
@@ -12,12 +12,12 @@ function Featuremap = Convlayer(images,W,b)
         for fil2 = 1:numFilters2
             convolvedImage = zeros(convDim, convDim);
             for fil1 = 1:numFilters1
-                filter = squeeze(W(:,:,fil1,fil2));
+                filter = squeeze(Weight(:,:,fil1,fil2));
                 filter = rot90(squeeze(filter),2);
                 im = squeeze(images(:,:,fil1,i));
                 convolvedImage = convolvedImage + conv2(im,filter,'valid');
             end
-            convolvedImage = bsxfun(@plus,convolvedImage,b(fil2));
+            convolvedImage = bsxfun(@plus,convolvedImage,Bias(fil2));
             convolvedImage = 1 ./ (1+exp(-convolvedImage));
             Featuremap(:, :, fil2, i) = convolvedImage;
         end
